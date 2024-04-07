48a58af1e1ac02a39fa973c762ee98ca2128bf4a
Nikita Riabov <atikinvobair@gmail.com>
2024-04-07 15:43:07 +0200

    idk

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -11,27 +11,6 @@
     plugins = with pkgs.vimPlugins; [
       lazy-nvim
     ];
-
-    extraLuaConfig = ''
-      vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
-      require("lazy").setup({
-        performance = {
-          reset_packpath = false,
-          rtp = {
-            reset = false,
-          }
-        },
-        dev = {
-        },
-        install = {
-          -- Safeguard in case we forget to install a plugin with Nix
-          missing = false,
-        },
-	spec = {
-          { import = "plugins" },
-	},
-      })
-    '';
   };
 
  
