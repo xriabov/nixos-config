afb99506aca1cb6993e03081796412e484582930
Nikita Riabov <atikinvobair@gmail.com>
2024-04-07 14:09:14 +0200

    zsh

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,6 +1,21 @@
-{ lib, pkgs, ... }: {
-  programs.bash = {
+{ config, lib, pkgs, ... }: {
+  programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellALiases = {
+      update = "sudo nixos-rebuild switch --flake /home/pathetic_gull/repos/nixos";
+    };
+
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [rust dotnet git golang];
+      theme = "robbyrussel";
+    };
   };
 }
