5d16725337bda0470a2ae60972a9ab8807e95331
Nikita Riabov <atikinvobair@gmail.com>
2024-04-07 15:55:11 +0200

    idk

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -2,42 +2,10 @@
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-  programs.neovim = {
+  
+  programs.nixvim = {
     enable = true;
-
-    viAlias = true;
-    vimAlias = true;
-
-    plugins = with pkgs.vimPlugins; [
-      lazy-nvim
-    ];
-
-    extraLuaConfig = ''
-      vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
-      require("lazy").setup({
-        performance = {
-          reset_packpath = false,
-          rtp = {
-            reset = false,
-          }
-        },
-        dev = {
-        },
-        install = {
-          -- Safeguard in case we forget to install a plugin with Nix
-          missing = false,
-        },
-	spec = {
-          { import = "plugins" },
-	},
-      })
-    '';
-  };
-
- 
-  xdg.configFile."nvim/lua" = {
-    recursive = true;
-    source = ./lua;
-  };
-
+    colorschemes.gruvbox.enable = true;
+    plugins.lightline.enable = true;
+  }
 }
