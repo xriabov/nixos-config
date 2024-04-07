723eaa2fad4c77b0909767a4b923396be5dd2c9c
Nikita Riabov <atikinvobair@gmail.com>
2024-04-07 15:48:30 +0200

    idk

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -2,7 +2,6 @@
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-
   programs.neovim = {
     enable = true;
 
@@ -10,8 +9,32 @@
     vimAlias = true;
 
     plugins = with pkgs.vimPlugins; [
+      lazy-nvim
     ];
+
+    extraLuaConfig = ''
+      vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+      require("lazy").setup({
+        performance = {
+          reset_packpath = false,
+          rtp = {
+            reset = false,
+          }
+        },
+        dev = {
+          path = "${pkgs.vimUtils.packDir config.home-manager.users.pathetic_gull.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+        },
+        install = {
+          -- Safeguard in case we forget to install a plugin with Nix
+          missing = false,
+        },
+	spec = {
+          { import = "plugins" },
+	},
+      })
+    '';
   };
+
  
   xdg.configFile."nvim/lua" = {
     recursive = true;
